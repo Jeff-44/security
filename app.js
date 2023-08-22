b0a8010d35614e7333ce34bf444d88b2cdc634d1
Jeff FORESTAL <forestaljeff00@gmail.com>
2023-08-22 13:18:38 -0400

    Start implementing OAuth token-based authentication

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 //jshint esversion:6
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-import ejs from "ejs";
-import mongoose from "mongoose";
-import session from "express-session";
-import passport from "passport";
-import passportLocalMongoose from "passport-local-mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import express from "express";
+// import ejs from "ejs";
+// import mongoose from "mongoose";
+// import session from "express-session";
+// import passport from "passport";
+// import passportLocalMongoose from "passport-local-mongoose";
+// import gs from "passport-google-oauth20";
+// import findOrCreate from "mongoose-findorcreate";
 
 
 
+// -----------------------------------------------------------------
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
-
+// -----------------------------------------------------------------
 const app = express();
 const port = 3000;
+// const GoogleStrategy = gs.Strategy;
 
 
 // MIDDLEWARES
@@ -44,11 +57,13 @@ app.use(passport.session());
 // USER SCHEMA || MODEL
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 // USE PASSPORT-LOCAL-MONGOOSE ON THE USERSCHEMA
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
@@ -56,9 +71,41 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// OAUTH FEATURE ADDED
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET_CODE,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
 app.get("/", (req, res)=>{
     res.render("home.ejs");
 });
+
+app.get("/auth/google", (req, res)=>{
+    try {
+        passport.authenticate("google", {scope: ["profile"]});
+        console.log("Successfully logged in with google");
+    } catch (error) {
+        console.log(error);
+        res.send(error.message);
+    }
+});
+
+app.get("/auth/google/secrets",
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res)=>{
+        res.redirect("/secrets");
+    }
+);
 
 app.get("/secrets", (req, res)=>{
     if(req.isAuthenticated()){
